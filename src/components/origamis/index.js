8f890bb779fe88b43d728c7ffc42e035ed134b11
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-07-17 19:46:34 +0300

    Update index.js

--- src/components/origamis/index.js
+++ src/components/origamis/index.js
@@ -1,48 +1,46 @@
 import React, { Component } from 'react'
-import Origam from '../origam'
 import styles from './index.module.css'
+import Origam from '../origam';
 
 class Origamis extends Component {
     constructor(props) {
         super(props)
 
-        this.setState = {
+        this.state = {
             origamis: []
         }
     }
 
     getOrigamis = async () => {
-        const promise = await fetch('http://localhost:9999/api/orimagi')
+        const promise = await fetch('http://localhost:9999/api/origami')
         const origamis = await promise.json()
-
         this.setState({
             origamis
         })
     }
 
     renderOrigamis() {
-        const origamis = this.state
-        return origamis.map(origam => {
+        const { origamis } = this.state
+
+        return origamis.map((origam, index) => {
             return (
-                <Origam key={origam._id} {...origam} />
+                <Origam key={origam._id} index={index} {...origam} />
             )
         })
-
     }
 
     componentDidMount() {
         this.getOrigamis()
     }
 
-
     render() {
         return (
             <div className={styles.container}>
-                <h1 className={styles.title}>Origamis</h1>
-                <div className={styles['origamis-wrapper']}>
+                <h1 className={styles.title}>Orgimis</h1>
+                <div className={styles["origamis-wrapper"]}>
                     {this.renderOrigamis()}
                 </div>
-            </div >
+            </div>
         )
     }
 }
