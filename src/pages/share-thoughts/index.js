3a9b46a93ef5a00abb371d8859d685c9614ac5ec
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-07-18 21:17:19 +0300

    add Submit button

--- src/pages/share-thoughts/index.js
+++ src/pages/share-thoughts/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import PageLayout from '../../components/page-layout'
 import styles from './index.module.css'
 import Title from '../../components/title'
+import SubmitButton from '../../components/button/submit-button'
 
 const ShareThoughtsPage = () => {
     return (
         <PageLayout>
-            <Title title='Share your thoughts...'/>
+            <Title title='Share your thoughts...' />
+            <textarea>Publication...</textarea>
+            <SubmitButton title="Submit" />
         </PageLayout>
     )
 }
