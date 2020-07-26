9c93ef3301771707ec8253a9c878e2b7863542d5
Alexander Todorov <41835213+tdrv90@users.noreply.github.com>
2020-07-26 16:47:17 +0300

    add auth

--- REST API/controllers/user.js
+++ REST API/controllers/user.js
@@ -50,6 +50,38 @@ module.exports = {
                     res.clearCookie(config.authCookieName).send('Logout successfully!');
                 })
                 .catch(next);
+        },
+
+        verifyLogin: (req, res, next) => {
+            const token = req.cookies[config.authCookieName] || '';
+
+            Promise.all([
+                utils.jwt.verifyToken(token),
+                models.TokenBlacklist.findOne({ token })
+            ])
+                .then(([data, blacklistToken]) => {
+                    if (blacklistToken) { return Promise.reject(new Error('blacklisted token')) }
+
+                    models.User.findById(data.id)
+                        .then((user) => {
+                            return res.send({
+                                status: true,
+                                user
+                            })
+                        });
+                })
+                .catch(err => {
+                    if (!redirectAuthenticated) { next(); return; }
+
+                    if (['token expired', 'blacklisted token', 'jwt must be provided'].includes(err.message)) {
+                        res.status(401).send('UNAUTHORIZED!');
+                        return;
+                    }
+
+                    return res.send({
+                        status: false
+                    });
+                })
         }
     },
 
